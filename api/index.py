309ac8558cc58e4d6ad2c01c35bd7c5abd73b36d
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2024-06-29 23:51:37 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,12 +4,30 @@ import json
 from flask import Flask
 import requests
 from bs4 import BeautifulSoup
+import pg8000
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     return 'Hello, World!'
+
+
+@app.route('/latest/<version>', methods=['GET'])
+def latest(version):
+    con = sqlite3.connect('windows.db')
+    cur = con.cursor()
+
+    sql = cur.execute('SELECT DISTINCT(patch) FROM windows WHERE release = "{release}" ORDER BY CAST(patch AS INTEGER) DESC'.format(release = version))
+
+    throwaway = sql.fetchone()[0]   # Requirement is latest-mnus-one
+    latest_patch = sql.fetchone()[0]
+    prior_patch = sql.fetchone()[0]
+
+    result = { "release":version, "latest_patch":latest_patch, "previous_patch":prior_patch, "previous_base":"10.0." + version + "." + prior_patch, "latest_base":"10.0." + version + "." + latest_patch  }
+
+    return json.dumps(result) + "\n"
+
 
 @app.route('/about')
 def about():
