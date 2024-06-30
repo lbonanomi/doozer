898f8c4897a5d02778693fc193a086777b59927a
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2024-06-30 00:16:23 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ import json
 from flask import Flask
 import requests
 from bs4 import BeautifulSoup
-import pg8000
+import pg8000.native
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ def home():
 
 @app.route('/latest/<version>', methods=['GET'])
 def latest(version):
-    connection = pg8000.connect(
+    connection = pg8000.native.Connection(
         host = os.environ['POSTGRES_HOST'],
         user = os.environ['POSTGRES_USER'],
         password = os.environ['POSTGRES_PASSWORD'],
@@ -23,16 +23,16 @@ def latest(version):
         port = 5432
     )
 
+    connection.close()
 
-    
-    con = sqlite3.connect('windows.db')
-    cur = con.cursor()
-
-    sql = cur.execute('SELECT DISTINCT(patch) FROM windows WHERE release = "{release}" ORDER BY CAST(patch AS INTEGER) DESC'.format(release = version))
-
-    throwaway = sql.fetchone()[0]   # Requirement is latest-mnus-one
-    latest_patch = sql.fetchone()[0]
-    prior_patch = sql.fetchone()[0]
+    #con = sqlite3.connect('windows.db')
+    #cur = con.cursor()
+    #
+    #sql = cur.execute('SELECT DISTINCT(patch) FROM windows WHERE release = "{release}" ORDER BY CAST(patch AS INTEGER) DESC'.format(release = version))
+    #
+    #throwaway = sql.fetchone()[0]   # Requirement is latest-mnus-one
+    #latest_patch = sql.fetchone()[0]
+    #prior_patch = sql.fetchone()[0]
 
     result = { "release":version, "latest_patch":latest_patch, "previous_patch":prior_patch, "previous_base":"10.0." + version + "." + prior_patch, "latest_base":"10.0." + version + "." + latest_patch  }
 
