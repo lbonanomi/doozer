b0cd8eecfd3bae5bccc42e7f60b817d2099cef8e
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2024-07-02 20:38:29 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,8 +2,6 @@ import os
 from time import strftime, localtime
 import json
 from flask import Flask
-#import requests
-#from bs4 import BeautifulSoup
 import pg8000.dbapi
 
 
@@ -27,25 +25,17 @@ def home():
 
 
 @app.route('/latest/<version>', methods=['GET'])
-def latest(version):
-#    connection = pg8000.dbapi.Connection(
-#        host = os.environ['POSTGRES_HOST'],
-#        user = os.environ['POSTGRES_USER'],
-#        password = os.environ['POSTGRES_PASSWORD'],
-#        database = os.environ['POSTGRES_DATABASE'],
-#        port = 5432
-#    )
-
     sql = connection.cursor()
     sql.execute("SELECT DISTINCT(patch) FROM windows WHERE release = '{release}' ORDER BY patch DESC".format(release = version))
 
-    throwaway = sql.fetchone()[0]   # Requirement is latest-minus-one
-    latest_patch = sql.fetchone()[0]
-    prior_patch = sql.fetchone()[0]
+    latest_patch = sql.fetchone()[0]   # Bold of you to beta the latest patch
+    stable_patch = sql.fetchone()[0]   # What most sensible folks deploy
+    prior_patch = sql.fetchone()[0]    # Acceptable for-now
+    authority = sql.fetchone()[0]      # Who says
     
     
     connection.close()
 
-    result = { "release":version, "latest_patch":latest_patch, "previous_patch":prior_patch, "previous_base":"10.0." + version + "." + prior_patch, "latest_base":"10.0." + version + "." + latest_patch  }
+    result = { "release":version, "latest_patch":latest_patch, "stable_patch":stable_patch, "previous_patch":prior_patch, "authority": authority  }
 
     return json.dumps(result) + "\n"
