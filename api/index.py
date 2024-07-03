098e6b7bc7fec726970bb75644edf11096023b82
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2024-07-02 22:05:14 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -32,18 +32,28 @@ def latest(version):
     latest_patch = sql.fetchone()
     latest_patch_number = latest_patch[0]   # Bold of you to beta the latest patch
     latest_patch_authority = latest_patch[1]
+    prior_patch_kb = ""
     
     stable_patch = sql.fetchone()
     stable_patch_number = stable_patch[0]   # What most sensible folks deploy
     stable_patch_authority = stable_patch[1]
+    prior_patch_kb = ""
     
     prior_patch = sql.fetchone()
     prior_patch_number = prior_patch[0]    # Acceptable for-now
     prior_patch_authority = prior_patch[1]
+    prior_patch_kb = ""
+    
 
     sql.close()
     connection.close()
 
-    result = { "release":version, "latest_patch":latest_patch_number, "stable_patch":stable_patch_number, "previous_patch":prior_patch_number, "authority": latest_patch_authority  }
+    #result = { "release":version, "latest_patch":latest_patch_number, "stable_patch":stable_patch_number, "previous_patch":prior_patch_number, "authority": latest_patch_authority  }
+    result = { 
+        "release":version, 
+        "latest": { "patch_number": latest_patch_number, "authority": latest_patch_authority, "kb": latest_patch_kb }, 
+        "stable": { "patch_number": stable_patch_number, "authority": stable_patch_authority, "kb": stable_patch_kb }, 
+        "previous": { "patch_number": prior_patch_number, "authority": prior_patch_authority, "kb": prior_patch_kb } 
+    }
 
     return json.dumps(result) + "\n"
