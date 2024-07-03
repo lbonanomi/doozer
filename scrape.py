07b2b3149ef9e31424ba14f06d6343e62539f13b
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2024-07-03 11:36:17 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,7 +4,71 @@ import psycopg2
 import os
 import re
 
-print(
-  len(os.environ['POSTGRES_USER']), 
-  len(os.environ['POSTGRES_PASSWORD'])
-     )
+
+pages = ["https://learn.microsoft.com/en-us/windows/release-health/windows11-release-information" , "https://learn.microsoft.com/en-us/windows/release-health/release-information"]
+
+connection = psycopg2.connect(
+  host = os.environ['POSTGRES_HOST'],
+  user = os.environ['POSTGRES_USER'],
+  password = os.environ['POSTGRES_PASSWORD'],
+  database = os.environ['POSTGRES_DATABASE'],
+  port = 5432,
+  sslmode='require'
+)
+
+print(len(user), len(password))
+
+sql = connection.cursor()
+
+
+for page in pages:
+  data = requests.get(page, timeout=10).text
+
+  soup = BeautifulSoup(data, 'html.parser')
+    
+  for table in soup.find_all('table'):
+
+    rows = table.find_all('tr')
+
+    for row in rows:
+      columns = row.find_all('td')
+
+      try:
+        svc_option = columns[0].text
+      except IndexError:
+        continue
+
+      try:
+        avail_date = columns[1].text
+      except IndexError:
+        continue
+
+      try:
+        build = columns[2].text
+
+        if "." in build:
+          (release, patch) = build.split(".")
+        else:
+          release = build
+          patch = ""
+
+      except IndexError:
+        continue
+
+        try:
+          kb = columns[3].text
+        except IndexError:
+          continue
+
+
+        if re.search("[0-9]{4}\-[0-9]{2}\-[0-9]{2}", avail_date):
+          print("INSERT INTO windows VALUES ('{}', '{}', '{}', '{}', '{}', '{}')".format(svc_option, avail_date, release, patch, page, kb))
+
+          sql.execute("INSERT INTO windows VALUES ('{}', '{}', '{}', '{}', '{}', '{}')".format(svc_option, avail_date, release, patch, page, kb))
+
+          #v = sql.fetchall()
+          #print(v)
+
+connection.commit()
+sql.close()
+connection.close()
