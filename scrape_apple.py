907197ab43219d9bdf1a9cc13f97b945af43587c
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2024-12-03 15:26:34 -0500

    Create scrape_apple.py

---
+++ scrape_apple.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import psycopg2
+import os
+import re
+
+connection = psycopg2.connect(
+  host = os.environ['POSTGRES_HOST'],
+  user = os.environ['POSTGRES_USER'],
+  password = os.environ['POSTGRES_PASSWORD'],
+  database = os.environ['POSTGRES_DATABASE'],
+  port = 5432,
+  sslmode='require'
+)
+
+sql = connection.cursor()
+
+
+releases = ['Sequoia', 'Sonoma', 'Ventura']
+
+for page in ['https://support.apple.com/en-us/100100']:
+    data = requests.get(page, timeout=10).text
+
+    soup = BeautifulSoup(data, 'html.parser')
+
+    for release in releases:
+        for table in soup.find_all('table'):
+            for row in table.find_all('tr'):
+
+                if "macOS" in row.text:
+                    columns = row.find_all('td')
+
+                    if release in columns[0].text:
+                        for links in columns[0].findAll('a'):
+                            stem = "https://support.apple.com" + links.get('href')
+
+                        patchArr = columns[0].text.split()
+                        patch = re.sub('[A-Za-z]', '', patchArr[2])
+
+                        sql.execute("INSERT INTO macos VALUES ('{}', '{}', '{}', '{}', '{}')".format(columns[2].text, patch, release, page, stem))
+
+connection.commit()
+sql.close()
+connection.close()
